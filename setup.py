96a7fdeb8650f26148c5cdc5c45a2f230d2150e5
bruino <bruino.one@gmail.com>
2021-04-17 22:55:59 -0300

    Building package distribution x3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dialogflow-response",
-    version="0.1.1",
+    version="0.1.2",
     author="bruino",
     author_email="bruno.sarverry@outlook.com",
     description=" Create webhook responses for Dialogflow with this small library in python.",
